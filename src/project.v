b0e7a7b55562b19d49b0c728c9506be87ec98c47
Default Application User <189062873+SiliconeGuide@users.noreply.github.com>
2025-09-07 18:36:13 +0200

    updated names

--- src/project.v
+++ src/project.v
@@ -20,4 +20,41 @@ module tt_um_3v_inverter_SiliconeGuide (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  5v_inverter 5v_inverter (
+        .VDD(VDPWR),
+        .VSS(VGND),
+        .input(ua[1]),
+        .output(ua[0])
+        );
+
+    // ties for the output enables
+    assign uo_out[0] = VGND;
+    assign uo_out[1] = VGND;
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
+
+endmodule
+
 endmodule
